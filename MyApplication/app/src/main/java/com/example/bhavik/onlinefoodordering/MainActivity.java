b61a824ecdd43b8ff700cc442d0603a393fbf58c
varun77 <varun@Varuns-MacBook-Pro.local>
2016-06-01 19:34:16 -0700

    With google places api

--- MyApplication/app/src/main/java/com/example/bhavik/onlinefoodordering/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/bhavik/onlinefoodordering/MainActivity.java
@@ -1,87 +1,94 @@
-package com.example.bhavik.onlinefoodordering;
+/*
+* Copyright 2013 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.example.google.playservices.placepicker;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.common.activities.SampleActivityBase;
+import com.example.android.common.logger.Log;
 
-    DataBaseHelper helper = new DataBaseHelper(this);
+import com.example.google.playservices.placepicker.cardstream.CardStream;
+import com.example.google.playservices.placepicker.cardstream.CardStreamFragment;
+import com.example.google.playservices.placepicker.cardstream.CardStreamState;
+import com.example.google.playservices.placepicker.cardstream.OnCardClickListener;
+import com.example.google.playservices.placepicker.cardstream.StreamRetentionFragment;
+
+public class MainActivity extends SampleActivityBase implements CardStream {
+    public static final String TAG = "MainActivity";
+    public static final String FRAGTAG = "PlacePickerFragment";
+
+    private CardStreamFragment mCardStreamFragment;
+
+    private StreamRetentionFragment mRetentionFragment;
+    private static final String RETENTION_TAG = "retention";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        FragmentManager fm = getSupportFragmentManager();
+        PlacePickerFragment fragment =
+                (PlacePickerFragment) fm.findFragmentByTag(FRAGTAG);
 
-    public void onButtonClick(View v)
-    {
-        if (v.getId() == R.id.Blogin)
-        {
-            EditText a = (EditText)findViewById(R.id.TFusername);
-            String str = a.getText().toString();
-
-            EditText b = (EditText)findViewById(R.id.TFpassword);
-            String str1 = b.getText().toString();
-
-            String password = helper.searchPass(str);
-            if (str1.equals(password))
-            {
-                Intent i = new Intent(MainActivity.this,Display.class);
-                i.putExtra("Username", str);
-                startActivity(i);
-            }
-            else
-            {
-                Toast temp = Toast.makeText(MainActivity.this , "Email and Password are not correct" , Toast.LENGTH_SHORT);
-                temp.show();
-            }
+        if (fragment == null) {
+            FragmentTransaction transaction = fm.beginTransaction();
+            fragment = new PlacePickerFragment();
+            transaction.add(fragment, FRAGTAG);
+            transaction.commit();
         }
 
-        if (v.getId() == R.id.Bsignuphere)
-        {
-            Intent i = new Intent(MainActivity.this,SignUp.class);
-            startActivity(i);
+        // Use fragment as click listener for cards, but must implement correct interface
+        if (!(fragment instanceof OnCardClickListener)){
+            throw new ClassCastException("PlacePickerFragment must " +
+                    "implement OnCardClickListener interface.");
+        }
+        OnCardClickListener clickListener = (OnCardClickListener) fm.findFragmentByTag(FRAGTAG);
+
+        mRetentionFragment = (StreamRetentionFragment) fm.findFragmentByTag(RETENTION_TAG);
+        if (mRetentionFragment == null) {
+            mRetentionFragment = new StreamRetentionFragment();
+            fm.beginTransaction().add(mRetentionFragment, RETENTION_TAG).commit();
+        } else {
+            // If the retention fragment already existed, we need to pull some state.
+            // pull state out
+            CardStreamState state = mRetentionFragment.getCardStream();
+
+            // dump it in CardStreamFragment.
+            mCardStreamFragment =
+                    (CardStreamFragment) fm.findFragmentById(R.id.fragment_cardstream);
+            mCardStreamFragment.restoreState(state, clickListener);
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public CardStreamFragment getCardStream() {
+        if (mCardStreamFragment == null) {
+            mCardStreamFragment = (CardStreamFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.fragment_cardstream);
+        }
+        return mCardStreamFragment;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        CardStreamState state = getCardStream().dumpState();
+        mRetentionFragment.storeCardStream(state);
     }
 }
