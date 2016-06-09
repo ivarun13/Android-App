f4a3d5e716af6b4749dd8b5b1b3c9533f522d76b
bbhavsa <bhavik_bhavsar@ymail.com>
2016-06-09 16:51:40 -0700

    register and login
    modified
    validation added

--- MyApplication/app/src/main/java/com/example/bhavik/onlinefoodordering/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/bhavik/onlinefoodordering/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bhavik.onlinefoodordering;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,27 +11,32 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     DataBaseHelper helper = new DataBaseHelper(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
+
+
+
 
     public void onButtonClick(View v)
     {
@@ -56,9 +62,16 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        if (v.getId() == R.id.Bsignuphere)
+
+
+
+
+
+       // TextView registerScreen = (TextView) findViewById(R.id.link_signup);
+
+        if (v.getId() == R.id.link_signup)
         {
-            Intent i = new Intent(MainActivity.this,SignUp.class);
+            Intent i = new Intent(getApplicationContext(),SignUp.class);
             startActivity(i);
         }
     }
